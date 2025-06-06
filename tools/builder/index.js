cd00dd9eaca67a5bdb3bacbd864900f4f2b72e38
Deb Axom <debaxom@gmail.com>
2025-06-07 00:28:08 +0530

    Fixed a bug in the Static Site Generator

--- tools/builder/index.js
+++ tools/builder/index.js
@@ -20,15 +20,26 @@ async function bundleJs(RootPath) {
     } catch (err) { }
 }
 
+async function ensureDirectoryExistence(filePath) {
+    let dirname = path.dirname(filePath);
+    if (fs.existsSync(dirname)) {
+      return true;
+    }
+    await ensureDirectoryExistence(dirname);
+    await fs.promises.mkdir(dirname);
+}
+
 export async function Build(RootPath) {
     // const RootPath = process.cwd();
     const BundledScript = await bundleJs(RootPath);
     const Template = await fs.promises.readFile(path.join(RootPath, 'index.html'), 'utf-8');
     
     await crawlDirectory(path.join(RootPath, 'src/pages'));
-    const urls = Files.map(file => {
-        if(typeof file === 'string'  && file.endsWith("/_'")) return;
-        getRouteFromFilePath(file).url.replace(/'/g, '')
+    const urls = [];
+    Files.forEach(file=>{
+        const route = getRouteFromFilePath(file).url.replace(/'/g, '');
+        if(route.endsWith("/_")) return;
+        urls.push(route);
     });
 
     urls.forEach(async url => {
@@ -39,9 +50,10 @@ export async function Build(RootPath) {
         if (!fs.existsSync(distDir)) await fs.promises.mkdir(distDir);
         if (!fs.existsSync(pagesDir)) await fs.promises.mkdir(pagesDir);
 
-
         url = url === "/" ? "index" : url;
+        let pageUrl = path.join(pagesDir, `${url}.html`);
 
-        await fs.promises.writeFile(path.join(pagesDir, `${url}.html`), minifyHTML(html));
+        await ensureDirectoryExistence(pageUrl);
+        await fs.promises.writeFile(pageUrl, minifyHTML(html));
     });
 }
